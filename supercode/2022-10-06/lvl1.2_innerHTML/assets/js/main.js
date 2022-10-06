10d7fd82f6ff40e1cf9fdc0ba792af3c86dfae23
YubiFuu <jess.supercode@gmail.com>
2022-10-06 14:26:06 +0200

    JS Intro Aufgaben

---
+++ supercode/2022-10-06/lvl1.2_innerHTML/assets/js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("info").innerHTML = "<h1>Hello World!</h1>";
+document.getElementById("info").innerHTML += "<h2>How are you today?</h2>";
+
+document.getElementById("container").innerHTML = "<p> start of element </p>";
+document.write("end of element");
---
+++ supercode/2022-10-06/lvl1.3_innerHTML/assets/js/main.js
@@ -0,0 +1,24 @@
+let img1 =
+    "<img src='https://images.unsplash.com/photo-1554364425-28f2d48c99a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGFzdHJvbm9taWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt='Trulli' style='width:200px'>";
+
+let img2 =
+    "<img src='https://images.unsplash.com/photo-1633377319967-a225355387ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' alt='Trulli' style='width:200px'>";
+
+let img3 =
+    "<img src='https://images.unsplash.com/photo-1633377183286-a5dddaadf066?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='Trulli' style='width:200px'>";
+
+document.getElementById("gallery").innerHTML =
+    "<figure style= 'display:flex' 'justify-content:space-between'>" +
+    "<div style= 'width:30vw'>" +
+    img1 +
+    "<figcaption>Fig.1</figcaption>" +
+    "</div>" +
+    "<div style= 'width:30vw'>" +
+    img2 +
+    "<figcaption>Fig.2</figcaption>" +
+    "</div>" +
+    "<div style= 'width:30vw'>" +
+    img3 +
+    "<figcaption>Fig.3</figcaption>" +
+    "</div>" +
+    "</figure>";

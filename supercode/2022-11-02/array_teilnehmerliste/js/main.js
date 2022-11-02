5c0efabaacf506b1bf81c03fa8524cb737023e70
YubiFuu <jess.supercode@gmail.com>
2022-11-02 14:12:33 +0100

    arrays

---
+++ supercode/2022-11-02/array_teilnehmerliste/js/main.js
@@ -0,0 +1,18 @@
+const btnAdd = document.getElementById("btn-add");
+const btnRemove = document.getElementById("btn-remove");
+const textInput = document.getElementById("text-input");
+const textOutput = document.querySelector("h2");
+
+let arrayText = [];
+
+btnAdd.addEventListener(`click`, (event) => {
+    event.preventDefault();
+    arrayText.push("&#129488; " + textInput.value + "</br>");
+
+    textOutput.innerHTML = arrayText.join("");
+});
+btnRemove.addEventListener(`click`, (event) => {
+    event.preventDefault();
+    arrayText.pop();
+    textOutput.innerHTML = arrayText.join("");
+});
